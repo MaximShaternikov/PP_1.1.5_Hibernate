970a408d55fecaddcae0e55fa64c97ffd3fc281a
MaximShaternikov <trickshm@gmail.com>
2024-05-21 03:18:55 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Mike", "Foster", (byte) 35);
+        userService.saveUser("Felix", "Armstrong", (byte) 24);
+        userService.saveUser("Mathew", "Jameson", (byte) 41);
+        userService.saveUser("Cory", "Barnes", (byte) 15);
+
+        userService.getAllUsers();
+
+        userService.removeUserById(1);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
+        Util.toClose();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/db";
+    private static final String USERNAME = "root_";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void toClose() {
+        try (Connection conn = getConnection()) {
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
